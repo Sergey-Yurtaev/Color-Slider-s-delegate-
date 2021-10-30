ea041949e3b5abfa1155b0fdb2bd54ff4f74fb23
Sergey-Yurtaev <engineerk2@mail.ru>
2021-10-30 15:37:38 +0300

    Initial Commit

---
+++ HW.2.2.1 Color Slider's/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2.2.1 Color Slider's
+//
+//  Created by Sergey Yurtaev on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
